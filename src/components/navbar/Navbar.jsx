841a628e35c01c5e9eab077ca0b57e7a84af2c4e
Krithin Jay Pakshootra <krithinjaycode@gmail.com>
2023-12-07 20:35:30 +0800

    add logo and first link

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,8 +1,20 @@
-import React from 'react';
-import './Navbar.css';
+import React from "react";
+import "./Navbar.css";
+import { NavLink } from "react-router-dom";
 
 const Navbar = () => {
-    return ( <><div className="navbar"></div></> );
-}
- 
+  return (
+    <>
+      <div className="navbar">
+        <p className="logo">Calcu</p>
+        <div className="nav-items">
+            <p className="nav-link">
+                <NavLink to="/">Home</NavLink>
+            </p>
+        </div>
+      </div>
+    </>
+  );
+};
+
 export default Navbar;
