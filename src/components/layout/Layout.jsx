d103aa03eac157a1191dd039e72dbe62dafeac23
Krithin Jay Pakshootra <krithinjaycode@gmail.com>
2023-12-07 20:39:42 +0800

    finish for now -  Layout.jsx

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -1,7 +1,15 @@
 import React from "react";
+import Navbar from "./Navbar";
 
 const Layout = () => {
-  return <></>;
+  return (
+    <>  
+        <div className="layout">
+            <Navbar />
+            <div className="page"></div>
+        </div>
+    </>
+    );
 };
 
 export default Layout;
