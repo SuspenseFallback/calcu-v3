7451cd0619c7cd3bae46b086020c25e98dc1b9e3
Krithin Jay Pakshootra <krithinjaycode@gmail.com>
2023-12-07 20:38:17 +0800

    create Layout.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import Navbar from "./components/navbar/Navbar.jsx";
+import Navbar from "./components/layout/Navbar.jsx";
 
 const router = createBrowserRouter([
   {
