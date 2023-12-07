95eef96a425aff921f7c3b50c97ecff33c8f0e56
Krithin Jay Pakshootra <krithinjaycode@gmail.com>
2023-12-07 20:44:43 +0800

    create Button.jsx

--- src/components/home/Wrapper.jsx
+++ src/components/home/Wrapper.jsx
@@ -2,7 +2,12 @@ import React from 'react';
 
 const HomeWrapper = () => {
     return ( <>
-    <div className="home-page-1"></div>
+    <div className="home-page-1">
+        <div className="card">
+            <p className="title">Practice</p>
+            <button className="button">Go</button>
+        </div>
+    </div>
     </> );
 }
  
